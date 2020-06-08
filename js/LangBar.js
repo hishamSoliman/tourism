8239d4ee672d5bc8a5ce661bbca7f35d44044afd
hishamSoliman <48646250+hishamSoliman@users.noreply.github.com>
2020-06-09 02:21:41 +0300

    Add files via upload

---
+++ js/LangBar.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $(".lang-flag").click(function () {
+        $(".language-dropdown").toggleClass("open");
+    });
+    $("ul.lang-list li").click(function () {
+        $("ul.lang-list li").removeClass("selected");
+        $(this).addClass("selected");
+        if ($(this).hasClass('lang-en')) {
+            $(".language-dropdown").find(".lang-flag").addClass("lang-en").removeClass("lang-Ar");
+        } else if ($(this).hasClass('lang-Ar')) {
+            $(".language-dropdown").find(".lang-flag").addClass("lang-Ar").removeClass("lang-en");
+        }
+        $(".language-dropdown").removeClass("open");
+    });
+})
